6076219be38fe8ed3539e6fb26bfe9aab8910183
ager001 <agermidenga@gmail.com>
2025-04-16 15:30:13 +0300

    I have done some editing

--- Sneaker shop project/src/checkout.js
+++ Sneaker shop project/src/checkout.js
@@ -1,4 +1,11 @@
 
+//we have defined our variables in our javascript and we have targeted their id
+let label = document.getElementById('label');
+let shoppingCart = document.getElementById('shopping-cart');
+
+
+
+
 let cartItems = JSON.parse(localStorage.getItem('cart')) || [];//Here the get retrieved from the browser's local storage and if there is no it becomes empty 
 
 //below the total cartCount gets displayed in our checkout page
